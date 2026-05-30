75fad9cfa362cd479eb21c109e4d79bfcaaa00fb
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-05-30 12:26:31 +0100

    Created Bookmark class and validating constructor fields

--- script.js
+++ script.js
@@ -4,9 +4,44 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
+import { getUserIds, getData, setData, clearData } from "./storage.js";
 
 window.onload = function () {
   const users = getUserIds();
   document.querySelector("body").innerText = `There are ${users.length} users`;
 };
+
+// Class for Bookmark object
+class Bookmark {
+  constructor(id, url, title, description, timestamp, likes) {
+    const validParams = constructorValidation(url, title, description);
+
+    this.id = id;
+    this.url = validParams.url;
+    this.title = validParams.title;
+    this.description = validParams.description;
+    this.timestamp = validParams.timestamp;
+    this.likes = likes;
+  }
+}
+
+// Validating constructor arguments
+function constructorValidation(url, title, description) {
+  if (typeof url !== "string" || url.trim() === "") {
+    alert("URL must be a non-empty string");
+  }
+
+  if (typeof title !== "string" || title.trim() === "") {
+    alert("Title must be a non-empty string");
+  }
+
+  if (typeof description !== "string" || description.trim() === "") {
+    alert("Description must be a non-empty string");
+  }
+
+  return {
+    url: url.trim(),
+    title: title.trim(),
+    description: description.trim(),
+  };
+}
